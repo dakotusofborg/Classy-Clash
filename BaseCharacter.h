c35f61c66a7a9516b9b6019e6b46440bb58f5cdc
Dakota Cahill <dakota.cahill@outlook.com>
2025-05-29 17:44:32 -0400

    moved members and methods into BaseCharacter | made variables protected

--- BaseCharacter.h
+++ BaseCharacter.h
@@ -1,13 +1,35 @@
-#ifndef BASE_CHARACTER_H
-#define BASE_CHARACTER_H
+#include "Raylib.h"
+
+#ifndef BASE_CHAERACTER_H
+#define BASE_CHAERACTER_H
 
 class BaseCharacter
 {
 public:
     BaseCharacter();
+    Vector2 getWorldPos() { return worldPos; }
+    void undoMovement(); // for future use, e.g., when the character goes out of bounds
+    Rectangle getCollisionRec();
+protected: 
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float updateTime{1.f / 12.f};
+    float speed{4.f};
+    float width{};
+    float height{};
+    float scale{4.f};
 
-private: 
-
+private:
 };
 
-#endif // BASE_CHAERACTER_H
+#endif // BASE_CHARACTER_H
--- Enemy.h
+++ Enemy.h
@@ -1,31 +1,13 @@
 #include "raylib.h"
+#include "BaseCharacter.h"
 #pragma once
 
-class Enemy
+class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
-    Vector2 getWorldPos() { return worldPos; }
     void tick(float deltaTime);
-    void undoMovement(); // for future use, e.g., when the character goes out of bounds
-    Rectangle getCollisionRec();
 
 private:
-    Texture2D texture{LoadTexture("characters/goblin_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/goblin_idle_spritesheet.png")};
-    Texture2D run{LoadTexture("characters/goblin_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{};
-    // 1 : facing right, -1 : facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    int maxFrames{6};
-    float updateTime{1.f / 12.f};
-    float speed{4.f};
-    float width{};
-    float height{};
-    float scale{4.f};
+   
 };
