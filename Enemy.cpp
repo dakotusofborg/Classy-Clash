e6d0fdfe3b2370064c1c4a6e89d23a22c6cda8b1
Dakota Cahill <dakota.cahill@outlook.com>
2025-05-29 23:23:02 -0400

    Updated the Enemy screenPos using the knights worldPos

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,3 +1,5 @@
+#include "raylib.h"
+#include "raymath.h"
 #include "Enemy.h"
 
 Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
@@ -8,9 +10,13 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     run = run_texture;
     width = texture.width / maxFrames;
     height = texture.height;
-} 
+}
 
 void Enemy::tick(float deltaTime)
 {
+    if (target)
+    {
+        screenPos = Vector2Subtract(worldPos, target->getWorldPos());
+    }
     BaseCharacter::tick(deltaTime);
 }
